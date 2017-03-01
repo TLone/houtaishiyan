c255852276c767519e5bf524b80895d5490cc158
LENOVO <LENOVO@LENOVO-PC>
2017-03-01 16:03:30 +0800

    2

---
+++ tesetgit2/source/view/index.ui.js
@@ -0,0 +1,11 @@
+/**
+ * @Author : ֹˮ
+ * @Timestamp : 2017-03-01
+ */
+var nf = sm("do_Notification");
+var btn_hello = ui("btn_hello");
+
+btn_hello.on("touch", function() {
+	nf.alert("Hello World !!!!!");
+});
+
